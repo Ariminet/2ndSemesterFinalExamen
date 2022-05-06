d2bd57dcfcff22e3a277a8e7f676ba721f8896a9
Emil Ari Minet <emil7015@gmail.com>
2022-05-06 11:04:30 +0200

    Add project files.

---
+++ 2ndSemesterFinalExamen/Game1.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+enum GameStates
+{
+	PreGame,
+	Menu,
+	InGame,
+	Upgrades,
+	Pause
+}
+namespace _2ndSemesterFinalExamen
+{
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		private GameStates gameState;
+
+		private GameDataBase GameDB = new GameDataBase();
+
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			GameDB.Initialize();
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			GameDB.readDataDB();
+		}
+
+		
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+
+			switch (gameState)
+			{
+				case GameStates.PreGame:
+					PreGame();
+					break;
+				case GameStates.Menu:
+					GameMenu();
+					break;
+				case GameStates.InGame:
+					InGame();
+					break;
+				case GameStates.Upgrades:
+					UpgradesMenu();
+					break;
+				case GameStates.Pause:
+					PauseGame();
+					break;
+				default:
+					break;
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+
+	
+
+		public void PreGame()
+		{
+
+		}
+		public void GameMenu()
+		{
+
+		}
+		public void InGame() 
+		{
+		
+		}
+		public void UpgradesMenu() 
+		{
+
+		}
+		public void PauseGame()
+		{
+
+		}
+
+
+
+	}
+
+}
---
+++ 2ndSemesterFinalExamen/GameDataBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace _2ndSemesterFinalExamen
+{
+
+    class GameDataBase
+    {
+        SqlConnection connection;
+        string connectionString;
+
+        public virtual void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            connectionString = ConfigurationManager.ConnectionStrings["GameDB.Properties.Settings.GameDBConnectionString"].ConnectionString;
+        
+        }
+
+
+
+        public void readDataDB()
+        {
+            string query = "INSERT INTO Test VALUES(@FirstName, @LastName)";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@FirstName", "PalleMikkelsen");
+                command.Parameters.AddWithValue("@LastName", "mikkelsen");
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+
+    }
+}
