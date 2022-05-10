64e57a6b3543b0696ebb428012dc8b0f20d285e3
Emil Ari Minet <emil7015@gmail.com>
2022-05-10 10:05:38 +0200

    added gameobject pattern

---
+++ 2ndSemesterFinalExamen/Transform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GamePatternsProject
+{
+    public class Transform
+    {
+        public Vector2 Position { get; set; } = Vector2.Zero;
+
+        public void Translate(Vector2 translation)
+        {
+            Position += translation;
+        }
+    }
+}
